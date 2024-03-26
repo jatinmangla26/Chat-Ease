3a6724e39c5d96a1ea2dbcbda19ec8b1a4f5902e
Jatin Mangla <jatin.mangla@etmoney.com>
2024-03-26 20:02:57 +0530

    script

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,28 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import cors from "cors";
-import { app,server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-app.use(cors({credentials:true}));
+app.use(cors({ credentials: true }));
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
 
 server.listen(PORT, () => {
   connectToMongoDB();
