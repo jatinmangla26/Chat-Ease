62c998470aed5e67d6f34877cac16da1ad1b646d
Jatin Mangla <jatin.mangla@etmoney.com>
2024-03-26 20:07:23 +0530

    changed url

--- frontend/context/SocketContext.jsx
+++ frontend/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export  const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if (userData) {
       console.log("Trying to connect a user")
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-ease-qx9h.onrender.com/", {
         query: {
           userId: userData._id,
         },
