be40ccdf024896ef56e39cca55dfe037b9d1f752
Jatin Mangla <jatin.mangla@etmoney.com>
2024-03-26 15:20:53 +0530

    search functinonlity

--- frontend/src/components/Sidebar/SearchInput.jsx
+++ frontend/src/components/Sidebar/SearchInput.jsx
@@ -1,13 +1,65 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import { setSelectedConversation } from "../../redux/conversationSlice.js";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
-	return (
-		<form className='flex items-center gap-2'>
-			<input type='text' placeholder='Search…' className='input input-bordered rounded-full' />
-			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
-				<IoSearchSharp className='w-6 h-6 outline-none' />
-			</button>
-		</form>
-	);
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+
+  const [search, setSearch] = useState("");
+  const [conversations, setConversations] = useState([]);
+
+  const getConversations = async () => {
+    try {
+      const res = await fetch(`http://localhost:5000/api/users`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          token: userData.token,
+        }),
+      });
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      setConversations(data);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search term must be at least 3 characters long");
+    }
+    getConversations();
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      dispatch(setSelectedConversation(conversation));
+      setSearch("");
+    } else toast.error("No such user found!");
+  };
+  return (
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search…"
+        className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+        <IoSearchSharp className="w-6 h-6 outline-none" />
+      </button>
+    </form>
+  );
 };
 export default SearchInput;
